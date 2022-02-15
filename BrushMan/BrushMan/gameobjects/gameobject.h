b578f7aae7fd555ac82180487a472f44fc70dbac
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2022-02-15 17:14:17 +0100

    added dynamic allocation for the gameobject playiable area

--- BrushMan/BrushMan/gameobjects/gameobject.h
+++ BrushMan/BrushMan/gameobjects/gameobject.h
@@ -1,3 +1,5 @@
+#include <iostream>
+
 enum ObjectSymbol : char
 {
 	OBJECT = 'x',
@@ -23,21 +25,49 @@ public:
 	{
 		_symbol = type;
 
-		area = new int* [height];
+		area = new char* [height];
 		for (short i = 0; i < height; i++)
 		{
-			area[i] = new int[width];
+			area[i] = new char[width];
+		}
+
+		for (int j = 0; j < height; j++)
+		{
+			std::cout << "\n";
+			for (int i = 0; i < width; i++)
+			{
+				area[j][i] = 32;
+				std::cout << "|" << area[j][i] << "|";
+			}
+		}
+	}
+
+	~GameObject()
+	{
+		for (short i = 0; i < height; i++)
+		{
+			delete[] area[i];
 		}
 	}
 
 	bool is_overlapping(GameObject go) { return get_area() == go.get_area(); }
+	//void onOverlap() = 0;
 
-	virtual void onOverlap() = 0;
 private:
-	int** area = nullptr;
+	char** area = nullptr;
 	unsigned short height = 0;
 	unsigned short width = 0;
 	char _symbol;
 
 	unsigned short get_area() { return height * width; }
 };
+
+int main() {
+
+	GameObject go(ObjectSymbol::PLAYER, 2, 4);
+
+
+	system("pause");
+	return 0;
+}
+
